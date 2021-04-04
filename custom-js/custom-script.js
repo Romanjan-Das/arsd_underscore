6adc00294ccf59c0373906d5dfbf0f21b68535bd
romanjan-das <romanjandas.mail@gmail.com>
2021-04-04 17:25:06 +0530

    added side blank spaces

---
+++ custom-js/custom-script.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+    var blank_div_1 = document.createElement("div");
+    blank_div_1.id="left_blank";
+    var blank_div_2 = document.createElement("div");
+    blank_div_2.id="right_blank";
+    var page = document.getElementById("page");
+    page.appendChild(blank_div_1);
+    page.appendChild(blank_div_2);
+}
